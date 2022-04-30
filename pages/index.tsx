7d547edf87527dda38e345dbe2cdf0bd32105fab
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-04-30 13:44:12 -0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+	return (
+		<div className="">
+			<Head>
+				<title>MONTZERA NFT Drop</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1>test </h1>
+		</div>
+	)
+}
+
+export default Home
+

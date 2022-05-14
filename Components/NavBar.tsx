2f1ca15b1f432ab49cbc8b582e60e999479c8652
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-05-14 19:59:49 -0400

    showing user profile when user is singed in with metamask

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -1,11 +1,15 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { Link } from 'react-scroll'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+
+
+
 
 const navigation = [
-	{ name: 'Home', href: 'Home', current: true },
+	{ name: 'Home', href: 'Home', current: false },
 	{ name: 'About', href: 'About', current: false },
 	{ name: 'Team', href: 'Team', current: false },
 ]
@@ -15,6 +19,8 @@ function classNames(...classes: any) {
 }
 
 export default function Navbar() {
+	const address = useAddress();
+	const disconnect = useDisconnect();
 	return (
 		<Disclosure as="nav" className="bg-gray-800 sticky top-0 z-10">
 			{({ open }) => (
@@ -73,25 +79,19 @@ export default function Navbar() {
 								</div>
 							</div>
 							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-								<button
-									type="button"
-									className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-								>
-									<span className="sr-only">View notifications</span>
-									<BellIcon className="h-6 w-6" aria-hidden="true" />
-								</button>
+
 
 								{/* Profile dropdown */}
 								<Menu as="div" className="ml-3 relative">
 									<div>
-										<Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+										{address ? <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
 											<span className="sr-only">Open user menu</span>
 											<img
 												className="h-8 w-8 rounded-full"
 												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 												alt=""
 											/>
-										</Menu.Button>
+										</Menu.Button> : null}
 									</div>
 									<Transition
 										as={Fragment}

9e30800dc58f8883f47d3ccc5190a5100602c04b
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-05-14 20:17:27 -0400

    better syntax for address condition and show address to user

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -84,14 +84,10 @@ export default function Navbar() {
 								{/* Profile dropdown */}
 								<Menu as="div" className="ml-3 relative">
 									<div>
-										{address ? <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+										{address && <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
 											<span className="sr-only">Open user menu</span>
-											<img
-												className="h-8 w-8 rounded-full"
-												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-												alt=""
-											/>
-										</Menu.Button> : null}
+											<p className='text-white'>Wallet Connected: {address.substring(0, 5)}...{address.length - 5}</p>
+										</Menu.Button>}
 									</div>
 									<Transition
 										as={Fragment}

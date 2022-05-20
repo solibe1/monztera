0906386b8a26df3caef9ab1a94852cd4c240677d
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-05-20 14:36:18 -0400

    adding gif of monsters
    wanted to do some fancy js work to loop and display some images but decided just to convert all images to one simple gif to save time, memory and space

--- Components/Mint.tsx
+++ Components/Mint.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-import monster1 from '../images/monster1.png'
-import monster2 from '../images/monster2.png'
-import monster3 from '../images/monster3.png'
-import monster4 from '../images/monster4.png'
-import monster5 from '../images/monster5.png'
-import monster6 from '../images/monster6.png'
+import monsters from '../images/monsters.gif'
+
 import { Button } from './Hero'
 
 
@@ -16,12 +12,21 @@ function Mint() {
 
 	return (
 
-		<div className='bg-gradient-to-r from-[#eecda3] to-[#ef629f] '>
-			<h1 className='text-6xl text-center font-bold py-3'>Mint your NFT</h1>
+
+		<div className='bg-gradient-to-r from-[#eecda3] to-[#ef629f]'>
+			<h1 className='text-6xl text-center font-bold pt-8 py-3'>Mint your
+				<span className='relative'>
+					<span className="block absolute -inset-1 -skew-y-3 bg-cyan-500"></span>
+
+					<span className="relative text-white">NFT</span>
+				</span>
+			</h1>
 
 			<div className='flex flex-col items-center space-y-6 '>
 				<div className=''>
-					<Image src={monster1} />
+
+
+					<Image src={monsters} />
 					<p className='text-center'>NFT claimed 0/20</p>
 
 				</div>
@@ -31,7 +36,7 @@ function Mint() {
 
 			</div>
 
-		</div>
+		</div >
 	)
 }
 

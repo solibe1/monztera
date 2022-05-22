5b0a90e3c6a536bbbaabd0dd0000ef950e45cf3f
solibe1 <55918425+solibe1@users.noreply.github.com>
2022-05-22 17:00:30 -0400

    Update Mint.tsx

--- Components/Mint.tsx
+++ Components/Mint.tsx
@@ -13,6 +13,7 @@ function Mint() {
 	const [totalSupply, setTotalSupply] = React.useState<BigNumber>()
 	const nftDrop = useNFTDrop("0x712F1513B241e84b2be0c1459E7198a4916a6467") //smart contract address of nft drop
 	const [loading, setLoading] = React.useState<boolean>(true)
+	const [price, setPrice] = React.useState<string>()
 
 
 	useEffect(() => {
@@ -23,9 +24,17 @@ function Mint() {
 			const total = await nftDrop.totalUnclaimedSupply()
 			setTotalSupply(total)
 			setLoading(false);
+			const claimConditions = await nftDrop.claimConditions.getAll()
+			setPrice(claimConditions?.[0].currencyMetadata.displayValue)
 		}
 		fetchNftData();
 	}, [nftDrop])
+
+
+	const mintNFT = () => {
+		console.log("minting")
+
+	}
 
 	return (
 
@@ -44,7 +53,7 @@ function Mint() {
 
 
 					<Image src={monsters} />
-					<p className='text-center w-auto  rounded-md bg-white py-3 px-4 font-poppins text-lg font-medium uppercase text-pink-600 shadow-lg  lg:mb-0'>
+					<p className={loading ? 'text-center w-auto animate-pulse  rounded-md bg-white py-3 px-4 font-poppins text-lg font-medium uppercase text-pink-600 shadow-lg  lg:mb-0' : "text-center w-auto  rounded-md bg-white py-3 px-4 font-poppins text-lg font-medium uppercase text-pink-600 shadow-lg  lg:mb-0"}>
 						{loading ? <>loading supply...</>
 							: <> NFT supplies left: {totalSupply?.toString()}</>
 
@@ -56,7 +65,7 @@ function Mint() {
 
 				</div>
 				<div className='pb-5'>
-					<Button>Mint NFT (0.01ETH)</Button>
+					<Button onClick={mintNFT}>Mint NFT ({price} ETH) </Button>
 				</div>
 
 			</div>
